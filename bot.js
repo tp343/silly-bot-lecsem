d31ce5bc9741e8e522d9fc1a9809e927f93d98b5
Tomas Petricek <tomas@tomasp.net>
2021-02-15 09:56:28 +0000

    Tweak code, add travis

--- bot.js
+++ bot.js
@@ -1,34 +1,38 @@
 var v = require('voca');
 
-// Answers a given query
+// Answers queries about (i) rain, sun or (ii) football
 function answerQuery(query) {
   var query_lower = v.lowerCase(query);
-  if (v.includes(query_lower, "rain") || v.includes(query_lower, "sun"))
-    { return "I do not care too much about weather"; }
-  if (!v.includes(query_lower, "football"))
-      { return "Sorry Dave, I cannot do that." }
-  return "Football is the ballet of the masses."
+  if (v.includes(query_lower, "rain") || v.includes(query_lower, "sun")) { 
+    return "I do not care too much about weather, I'm locked inside a data center."; 
+  }
+  if (v.includes(query_lower, "tea")) { 
+    return "I would love some tea, but they have not created one for silicon-based life forms yet."; 
+  }
+  if (!v.includes(query_lower, "football")) { 
+    return "Sorry Dave, I cannot do that.";
+  }
+  return "Football is the ballet of the masses.";
 }
 
-// Handles a say click
-function HandleSayClick()
-{
-  var el = document.getElementById('message');
-  var ml = el.value.toLowerCase();
-  var cv = document.getElementById('conversation');
-  var an = answerQuery(ml)
-           conversation.innerHTML += "<p>" + ml + "</p>";
-           conversation.innerHTML += "<p>" + an + "</p>";
+// Responds to a message in the 'message' element by 
+// appending bot's response to the 'conversation' element
+function handleSayClick() {
+  var messageElement = document.getElementById('message');
+  var message = messageElement.value.toLowerCase();
+  var conversationElement = document.getElementById('conversation');
+  var answer = answerQuery(message);
+  conversationElement.innerHTML += "<p><em>" + message + "</em></p>";
+  conversationElement.innerHTML += "<p>" + answer + "</p>";
 }
 
 // If we are running in a browser, add onclick handler 
 // (if we are running tests, this will not do anything)
 if (typeof document != "undefined") {
-  document.getElementById("sayit").onclick = HandleSayClick;
+  document.getElementById("sayit").onclick = handleSayClick;
 }
 
 // Export the two functions that we need to access from test files!
 module.exports = { 
   answerQuery: answerQuery, 
-  isPermitted: isPermitted
 }
